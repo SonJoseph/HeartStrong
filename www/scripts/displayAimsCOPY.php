ca152bbaea2fb24abb46e7448ced40cd615d990d
ivysat <31290932+ivysat@users.noreply.github.com>
2018-04-05 16:23:49 -0400

    Slight aims changes
    Adding check to make sure form isn't blank, working on showing what is in the Aims that matches the patient ID

--- www/scripts/displayAimsCOPY.php
+++ www/scripts/displayAimsCOPY.php
@@ -1,6 +1,18 @@
 <?php
-require 'connect.php'
+require 'connect.php';
 
-$patientAims = "SELECT * FROM PatientID WHERE PatientID LIKE '%1%'""
+$sql = "SELECT AimName FROM Aims WHERE PatientID=1";
+$result = $conn->query($sql);
+
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    echo $row["AimName"]."<br>";
+  }
+} else {
+  echo "You haven't input any aims yet!";
+}
+
+mysqli_close($db);
 
 ?>
